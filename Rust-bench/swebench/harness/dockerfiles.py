56fdd077eafc0a02afdd874bc521717512a3e3d0
mjgaughan <58380965+mjgaughan@users.noreply.github.com>
2026-04-06 19:44:50 -0500

    adding cli flag to toggle between rustup/crates and mirrors when building rustbench eval containers

--- Rust-bench/swebench/harness/dockerfiles.py
+++ Rust-bench/swebench/harness/dockerfiles.py
@@ -5,6 +5,50 @@ FROM --platform={platform} ubuntu:20.04
 ARG DEBIAN_FRONTEND=noninteractive
 ENV TZ=Etc/UTC
 
+ENV RUSTUP_DIST_SERVER="https://static.rust-lang.org"
+ENV RUSTUP_UPDATE_ROOT="https://static.rust-lang.org/rustup"
+
+RUN apt update && apt install -y \
+wget \
+git \
+build-essential \
+libffi-dev \
+libtiff-dev \
+jq \
+curl \
+locales \
+locales-all \
+tzdata \
+&& rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update && \
+    apt-get install -y libssl-dev
+
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+ENV PATH=/root/.cargo/bin:$PATH
+RUN git clone https://github.com/riverLaugh/LTS.git
+RUN cd LTS && git checkout main && cargo install --path . 
+
+RUN git clone --bare https://github.com/rust-lang/crates.io-index-archive.git
+ENV CARGO_REGISTRY_GIT_DIR=/crates.io-index-archive.git
+
+RUN adduser --disabled-password --gecos 'dog' nonroot
+
+
+RUN apt-get update
+RUN apt-get install pkg-config -y
+RUN apt-get install python3-pip -y
+RUN apt-get install cmake -y
+RUN apt-get install protobuf-compiler -y
+
+"""
+
+_DOCKERFILE_BASE_MIRRORS = r"""
+FROM --platform={platform} ubuntu:20.04
+
+ARG DEBIAN_FRONTEND=noninteractive
+ENV TZ=Etc/UTC
+
 ENV RUSTUP_DIST_SERVER="https://rsproxy.cn"
 ENV RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
@@ -48,11 +92,29 @@ _DOCKERFILE_ENV = r"""FROM --platform={platform} rustb.base.{arch}:latest
 RUN mkdir -p ~/.cargo && \
     cat <<EOF > ~/.cargo/config
 [source.crates-io]
+registry = "sparse+https://index.crates.io/"
+
+[net]
+git-fetch-with-cli = true
+EOF
+
+COPY ./setup_env.sh /root/
+RUN chmod +x /root/setup_env.sh
+RUN /bin/bash -c "source ~/.bashrc && /root/setup_env.sh"
+WORKDIR /testbed/
+
+# Automatically activate the testbed environment
+# RUN echo "source /opt/miniconda3/etc/profile.d/conda.sh && conda activate testbed" > /root/.bashrc
+"""
+
+_DOCKERFILE_ENV_MIRRORS = r"""FROM --platform={platform} rustb.base.{arch}:latest
+
+RUN mkdir -p ~/.cargo && \
+    cat <<EOF > ~/.cargo/config
+[source.crates-io]
 replace-with = 'rsproxy'
 [source.rsproxy]
 registry = "https://rsproxy.cn/crates.io-index"
-[source.rsproxy-sparse]
-registry = "sparse+https://rsproxy.cn/index/"
 [registries.rsproxy]
 index = "https://rsproxy.cn/crates.io-index"
 [net]
@@ -82,8 +144,41 @@ FROM --platform={platform} asterinas/asterinas:{tag}
 # ENV RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
 # ENV RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 
-ENV RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
-ENV RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
+ENV RUSTUP_DIST_SERVER="https://static.rust-lang.org"
+ENV RUSTUP_UPDATE_ROOT="https://static.rust-lang.org/rustup"
+
+RUN mkdir -p ~/.cargo && \
+    cat <<EOF > ~/.cargo/config
+[source.crates-io]
+registry = "sparse+https://index.crates.io/"
+#replace-with = "sjtu"
+
+#[source.tuna]
+#registry = "https://mirrors.tuna.tsinghua.edu.cn/crates.io-index"
+
+
+#[source.ustc]
+#registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+
+
+#[source.sjtu]
+#registry = "https://mirrors.sjtug.sjtu.edu.cn/git/crates.io-index"
+
+# rustcc社区
+#[source.rustcc]
+#registry = "git://crates.rustcc.cn/crates.io-index"
+EOF
+
+COPY ./setup_env.sh /root/
+RUN chmod +x /root/setup_env.sh
+RUN /bin/bash -c "source ~/.bashrc && /root/setup_env.sh"
+"""
+
+_DOCKERFILE_ENV_asterinas_MIRRORS = r"""
+FROM --platform={platform} asterinas/asterinas:{tag}
+
+ENV RUSTUP_DIST_SERVER="https://rsproxy.cn"
+ENV RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
 RUN mkdir -p ~/.cargo && \
     cat <<EOF > ~/.cargo/config
@@ -113,21 +208,24 @@ RUN /bin/bash -c "source ~/.bashrc && /root/setup_env.sh"
 
 
 
-def get_dockerfile_base(platform, arch):
+def get_dockerfile_base(platform, arch, use_official_mirrors=True):
     if arch == "arm64":
         conda_arch = "aarch64"
     else:
         conda_arch = arch
-    return _DOCKERFILE_BASE.format(platform=platform, conda_arch=conda_arch)
+    template = _DOCKERFILE_BASE if use_official_mirrors else _DOCKERFILE_BASE_MIRRORS
+    return template.format(platform=platform, conda_arch=conda_arch)
 
 
-def get_dockerfile_env(platform, arch):
-    return _DOCKERFILE_ENV.format(platform=platform, arch=arch)
+def get_dockerfile_env(platform, arch, use_official_mirrors=True):
+    template = _DOCKERFILE_ENV if use_official_mirrors else _DOCKERFILE_ENV_MIRRORS
+    return template.format(platform=platform, arch=arch)
 
 
 def get_dockerfile_instance(platform, env_image_name):
     return _DOCKERFILE_INSTANCE.format(platform=platform, env_image_name=env_image_name)
 
 
-def get_dockerfile_env_asterinas(platform, tag):
-    return _DOCKERFILE_ENV_asterinas.format(platform=platform, tag=tag)
+def get_dockerfile_env_asterinas(platform, tag, use_official_mirrors=True):
+    template = _DOCKERFILE_ENV_asterinas if use_official_mirrors else _DOCKERFILE_ENV_asterinas_MIRRORS
+    return template.format(platform=platform, tag=tag)
